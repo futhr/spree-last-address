b3111962571d4874b4d78096fab9b14924e33e87
Tyler Rick <tyler@k3integrations.com>
2011-04-01 16:38:35 -0700

    default_country is now default_country_id in the latest version of Spree

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -8,8 +8,8 @@ CheckoutController.class_eval do
     #puts "BEFORE ADDRESS2 called user= #{@order}  #{@order.email}"
     @order.bill_address , @order.ship_address = FindAddressHelper.find_address(@order.email)
     # in case none found
-    @order.bill_address ||= Address.new(:country => default_country)
-    @order.ship_address ||= Address.new(:country => default_country)
+    @order.bill_address ||= Address.new(:country_id => default_country_id)
+    @order.ship_address ||= Address.new(:country_id => default_country_id)
   end
 
 
