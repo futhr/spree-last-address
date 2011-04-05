ba226294d93577282e0447e49499629d08491409
Tyler Rick <tyler@k3integrations.com>
2011-04-04 20:21:10 -0700

    If no last address is found in the user's order history, use the *user's* billing address (current_user.bill_address) if it is available.

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -9,8 +9,8 @@ CheckoutController.class_eval do
     #puts "BEFORE ADDRESS2 called user= #{@order}  #{@order.email}"
     @order.bill_address , @order.ship_address = FindAddressHelper.find_address(@order.email)
     # in case none found
-    @order.bill_address ||= current_user.bill_address || Address.new(:country_id => default_country_id)
-    @order.ship_address ||= current_user.ship_address || Address.new(:country_id => default_country_id)
+    @order.bill_address ||= current_user.bill_address || Address.new(:country_id => Spree::Config[:default_country_id])
+    @order.ship_address ||= current_user.ship_address || Address.new(:country_id => Spree::Config[:default_country_id])
   end
 
 
