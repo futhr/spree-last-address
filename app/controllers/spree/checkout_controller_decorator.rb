984466b30c63fb0430d309c24ec74e3ffdbae558
Jonny Dalgliesh <fighella@gmail.com>
2013-06-19 00:24:59 +1000

    fixed spree_current

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,7 +6,7 @@ module Spree
     def before_address
       return if @order.bill_address or @order.ship_address
       last_used_bill_address, last_used_ship_address = SpreeLastAddress.find_last_used_addresses(@order.email)
-      preferred_bill_address, preferred_ship_address = current_user.bill_address, current_user.ship_address if current_user.respond_to?(:bill_address) && current_user.respond_to?(:ship_address)
+      preferred_bill_address, preferred_ship_address = spree_current_user.bill_address, current_user.ship_address if current_user.respond_to?(:bill_address) && current_user.respond_to?(:ship_address)
       @order.bill_address ||= preferred_bill_address || last_used_bill_address || Address.default
       @order.ship_address ||= preferred_ship_address || last_used_ship_address || Address.default
     end
