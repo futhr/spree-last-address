63e917af687d70e45c443383d9bac79a50f97874
Tobias Bohwalli <hi@futhr.io>
2014-04-06 13:12:42 +0200

    Separation of spec support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,41 +15,12 @@ ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
 require 'rspec/rails'
-require 'capybara/rspec'
-require 'capybara/rails'
-require 'capybara/poltergeist'
 require 'ffaker'
-require 'database_cleaner'
-
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
-
-require 'spree/testing_support/factories'
-require 'spree/testing_support/controller_requests'
-require 'spree/testing_support/authorization_helpers'
-require 'spree/testing_support/url_helpers'
-require 'spree/testing_support/capybara_ext'
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-  config.include Spree::TestingSupport::ControllerRequests
-  config.include Spree::TestingSupport::UrlHelpers
-
   config.mock_with :rspec
   config.use_transactional_fixtures = false
-
-  config.before :suite do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.before do
-    DatabaseCleaner.strategy = example.metadata[:js] ? :truncation : :transaction
-    DatabaseCleaner.start
-  end
-
-  config.after do
-    DatabaseCleaner.clean
-  end
-
-  Capybara.javascript_driver = :poltergeist
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 end
+
+Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
