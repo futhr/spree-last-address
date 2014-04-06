8d4cc60e375d03e87bce27d7b9fc8945725215cc
Tobias Bohwalli <hi@futhr.io>
2014-04-06 14:01:41 +0200

    Move all methods into controller decorator.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -5,11 +5,24 @@ module Spree
 
     def before_address
       return if @order.bill_address or @order.ship_address
-      last_used_bill_address, last_used_ship_address = SpreeLastAddress.find_last_used_addresses(@order.email)
-      preferred_bill_address, preferred_ship_address = spree_current_user.bill_address, spree_current_user.ship_address if spree_current_user.respond_to?(:bill_address) && spree_current_user.respond_to?(:ship_address)
+
+      last_used_bill_address, last_used_ship_address = find_last_used_addresses(@order.email)
+
+      if spree_current_user.respond_to?(:bill_address) && spree_current_user.respond_to?(:ship_address)
+        preferred_bill_address, preferred_ship_address = spree_current_user.bill_address, spree_current_user.ship_address
+      end
 
       @order.bill_address ||= preferred_bill_address || last_used_bill_address || Address.default
       @order.ship_address ||= preferred_ship_address || last_used_ship_address || Address.default
+    end
+
+    def find_last_used_addresses(email)
+      past = Spree::Order.order("id desc").where(email: email).where("state != 'cart'").limit(8)
+      if order = past.detect(&:bill_address)
+        bill_address = order.bill_address.clone if order.bill_address
+        ship_address = order.ship_address.clone if order.ship_address
+      end
+      return bill_address, ship_address
     end
   end
 end
--- lib/spree_last_address/engine.rb
+++ lib/spree_last_address/engine.rb
@@ -14,15 +14,4 @@ module SpreeLastAddress
 
     config.to_prepare &method(:activate).to_proc
   end
-
-  class << self
-    def find_last_used_addresses(email)
-      past = Spree::Order.order("id desc").where(email: email).where("state != 'cart'").limit(8)
-      if order = past.detect(&:bill_address)
-        bill_address = order.bill_address.clone if order.bill_address
-        ship_address = order.ship_address.clone if order.ship_address
-      end
-      return bill_address, ship_address
-    end
-  end
 end
