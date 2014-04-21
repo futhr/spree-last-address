72dcbe5b64b5a31372255de59b7cd6d86bc9881f
Tobias Bohwalli <hi@futhr.io>
2014-04-21 18:44:54 +0200

    Fix instance variable @before_address not init

--- spec/controllers/spree/checkout_controller_spec.rb
+++ spec/controllers/spree/checkout_controller_spec.rb
@@ -21,13 +21,13 @@ describe Spree::CheckoutController do
     it 'return nil if order.bill_address is not nil' do
       order.bill_address = create(:address)
       spree_get :edit, state: 'address'
-      expect(controller.instance_eval{ @before_address }).to be_nil
+      expect(controller.instance_eval{ before_address }).to be_nil
     end
 
     it 'return nil if order.ship_address is not nil' do
       order.ship_address = create(:address)
       spree_get :edit, state: 'address'
-      expect(controller.instance_eval{ @before_address }).to be_nil
+      expect(controller.instance_eval{ before_address }).to be_nil
     end
 
     it 'return last saved bill_address and ship_address of order' do
@@ -44,7 +44,7 @@ describe Spree::CheckoutController do
       order.bill_address = nil
       order.ship_address = nil
       spree_get :edit, state: 'address'
-      controller.instance_eval{ @before_address }
+      controller.instance_eval{ before_address }
       expect(order.bill_address).not_to be_nil
     end
   end
